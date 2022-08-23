chornerman/Survey

[#3] Implement UI for Login screen

lib/assets.dart
@@ -1,0 +1,4 @@
+class Assets {
+  static const String loginBackground = "assets/images/bg_login.png";
+  static const String nimbleLogo = "assets/images/ic_nimble.svg";
+}

chornerman/survey-flutter

lib/assets.dart
@@ -1,0 +1,4 @@
+class Assets {
+  static const String loginBackground = "assets/images/bg_login.png";
+  static const String nimbleIcon = "assets/images/ic_nimble.svg";
+}